278602e3094a2bd1b2940452d06bc622cbe82cc6
Tadej Ferk <tadex121@gmail.com>
2021-02-17 16:15:45 +0100

    deploy react app

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, HashRouter, Route } from 'react-router-dom'
 import About from './components/about'
 import Home from './components/home'
 import Projects from './components/projects'
-import Contact from './components/contact'
+import ContactMe from './components/contactMe'
 
 function App() {
   return (
@@ -17,7 +17,7 @@ function App() {
         <Route path='/' exact component={Home} />
         <Route path='/about' component={About} />
         <Route path='/projects' component={Projects} />
-        <Route path='/contact' component={Contact} />
+        <Route path='/contact' component={ContactMe} />
       </HashRouter>
     </>
   )
